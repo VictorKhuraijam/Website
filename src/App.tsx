9d21f5c552392e03a57891b32d2cd5f2f9a3d1aa
Victorkhuraijam <khuraijamvictor@gmail.com>
2025-04-07 11:20:39 +0530

    initail commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-amber-300 text-blue-600'>Hello World! First typescript in react</h1>
+    </>
+  )
+}
+
+export default App
