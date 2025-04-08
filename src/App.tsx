7b3c851b565c122e0c5f34cd4b35fcaf8563b879
Victorkhuraijam <khuraijamvictor@gmail.com>
2025-04-08 23:03:59 +0530

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 
-import './App.css'
+
 
 function App() {
 
   return (
     <>
-      <h1 className='bg-amber-300 text-blue-600'>Hello World! First typescript in react</h1>
+      <h1 className='bg-amber-300 text-blue-600 text-center text-2xl '>Hello World! First typescript in react</h1>
     </>
   )
 }
