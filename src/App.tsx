496c73b2356d85c9691f88ac5454e6ce77e2737b
Victorkhuraijam <khuraijamvictor@gmail.com>
2025-04-09 22:28:18 +0530

    added components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-
+import {Route, Routes} from 'react-router-dom'
+import {ToastContainer} from 'react-toastify'
+import HomePage from './pages/Home'
 
 
 function App() {
 
   return (
     <>
-      <h1 className='bg-amber-300 text-blue-600 text-center text-2xl '>Hello World! First typescript in react</h1>
+      <ToastContainer />
+			{/* <Cursor /> */}
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+			</Routes>
     </>
   )
 }
---
+++ src/components/index.ts
@@ -0,0 +1,20 @@
+import Contact from "./Contact/Contact";
+import Footer from "./Footer/Footer";
+import Nav from './Header/Navbar'
+import About from './About'
+import Hero from './Hero'
+import Projects from './Projects'
+import Qualification from "./Qualifications";
+import Skills from "./Skills";
+
+
+export {
+  Contact,
+  Footer,
+  Nav,
+  About,
+  Hero,
+  Projects,
+  Qualification,
+  Skills
+}
