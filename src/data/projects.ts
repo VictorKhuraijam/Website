10378ea13993b9b70cb23f663db9a5c4457003d6
Victorkhuraijam <khuraijamvictor@gmail.com>
2025-06-04 09:19:40 +0530

    minor changes

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,6 +1,5 @@
 import RoyaleRex from '../assets/projects/RoyaleRex/Ecomerce.png'
-import gentile from "../assets/projects/gentile/main1.jpg";
-import lego from "../assets/projects/lego-homage/main1.png";
+
 
 export type ProjectType = {
 	id: number;
@@ -27,29 +26,6 @@ export const projects: ProjectType[] = [
 		link: "https://pet-store-d7dr.vercel.app/",
 		filter: ["All", "Web Development"],
 		type: "Web Development",
-	},
-  {
-		id: 1,
-		title: "Gentile Etiquetas Website",
-		about: "Wordpress custom theme",
-		description: "",
-		thumbnail: gentile,
-		tech: ["Wordpress", "PHP", "JavaScript"],
-		github: "",
-		link: "http://gentileetiquetas.com.br",
-		filter: ["All", "Web Development"],
-		type: "Web Development",
-	},
-  {
-  id: 2,
-  title: "LEGO Homage Piece",
-  about: "Homage project of LEGO.com",
-  description: "",
-  thumbnail: lego,
-  tech: ["HTML", "CSS", "SASS"],
-  github: "https://github.com/Gunnar50/lego-index-clone",
-  link: "https://lego-homage-gustavo-passarella.netlify.app/",
-  filter: ["All", "Web Development"],
-  type: "Frontend",
-  },
+	}
+
 ]
